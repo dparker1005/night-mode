3b83c831cf738380dad32fe9cfd486cf8b5c2569
David Parker <dparker837@verizon.net>
2017-06-28 20:54:01 -0400

    Worked on admin page, implemented loading from SQL. Saving settings still doesn't work

--- night-mode.php
+++ night-mode.php
@@ -25,8 +25,7 @@ function nmAddSwitch(){
 	 */
 	if ( is_null($screen) ){
 	?>
-	<script>console.log("rip");</script>
-	<div style="width:30px;">
+	<div style="width:200px; height:100px;">
 	<table><tr><th>Night Mode</th>
 	<td>
 		
@@ -100,23 +99,17 @@ function nmAddSwitch(){
 	<script>
 	jQuery(document).ready(function($){
   		//you can now use $ as your jQuery object.
-  		var nightmode = $( '#nightmode' );
-  		nightmode.click(function(){
+  		$( '#nightmode' ).click(function(){
   		var sheet = window.document.styleSheets[0]
 
-        if(document.getElementById('nightmode_checkbox').checked)
-        {
-        	console.log("Checked");
-        	//If you add image, it won't be affected
+        if(document.getElementById('nightmode_checkbox').checked){
 			sheet.insertRule('html, video {-webkit-filter: invert(1) hue-rotate(180deg);filter: invert(1) hue-rotate(180deg);background: black;}', sheet.cssRules.length);
 			
         }
-        else
-        {
-        	console.log("Unchecked");
+        else{
         	sheet.insertRule('html, video {-webkit-filter: invert(0) hue-rotate(0deg);filter: invert(0) hue-rotate(0deg);}', sheet.cssRules.length);
         }
-	});
+		});
 	});
 	</script>
 <?php
@@ -127,7 +120,165 @@ add_action('the_post', 'nmAddSwitch');
 
 //Adds menu to settings
 function nm_settings_page(){
-	echo "got it";
+	$nm_auto_place = get_option("nm_auto_place", NULL);
+	if(is_null($nm_auto_place)){
+		update_option("nm_auto_place", true);
+		$nm_auto_place = true;
+	}
+	
+	$nm_affect_pictures = get_option("nm_affect_pictures", NULL);
+	if(is_null($nm_affect_pictures)){
+		update_option("nm_affect_pictures", true);
+		$nm_affect_pictures = true;
+	}
+	
+	$nm_auto_set = get_option("nm_auto_set", NULL);
+	if(is_null($nm_auto_set)){
+		update_option("nm_auto_set", false);
+		$nm_auto_set = false;
+	}
+	
+	$nm_set_manually = get_option("nm_set_manually", NULL);
+	if(is_null($nm_set_manually)){
+		update_option("nm_set_manually", false);
+		$nm_set_manually = false;
+	}
+	
+	$nm_start_time = get_option("nm_start_time", NULL);
+	if(is_null($nm_start_time)){
+		update_option("nm_start_time", "20:00");
+		$nm_start_time = "20:00";
+	}
+	$nm_end_time = get_option("nm_end_time", NULL);
+	if(is_null($nm_end_time)){
+		update_option("nm_end_time", "06:00");
+		$nm_end_time = "06:00";
+	}
+	$nm_invert_color = get_option("nm_invert_color", NULL);
+	if(is_null($nm_invert_color)){
+		update_option("nm_invert_color", true);
+		$nm_invert_color = true;
+	}
+	
+	$nm_grey = get_option("nm_grey", NULL);
+	if(is_null($nm_grey)){
+		update_option("nm_grey", false);
+		$nm_grey = false;
+	}
+	
+	$nm_sepia = get_option("nm_sepia", NULL);
+	if(is_null($nm_sepia)){
+		update_option("nm_sepia", false);
+		$nm_sepia = false;
+	}
+
+	
+	?>
+	<h1>Night Mode Settings</h1>
+	<div style="background-color:white; width:400px;";>
+		<h2>General Settings</h2>
+		<table>
+			<tr>
+				<td>Automatically Place Switches</td>
+				<td><input type="checkbox" id="nm_auto_place" <?php if($nm_auto_place == true){ echo "checked";} ?>>
+				</td>
+			</tr>
+			<tr>
+				<td>Affect Pictures</td>
+				<td><input type="checkbox" id="nm_change_pics" <?php if($nm_affect_pictures == true){ echo "checked";} ?>></td>
+			</tr>
+			<tr>
+				<td>Enable Night Mode Automatically</td>
+				<td><input type="checkbox" id="nm_auto_set" <?php if($nm_auto_set == true){ echo "checked";} ?>></td>
+			</tr>
+			<tr id="nm_set_manually_tr">
+				<td style="padding-left:3em">Set Times Manually</td>
+				<td><input type="checkbox" id="nm_set_manually" <?php if($nm_set_manually == true){ echo "checked";} ?>></td>
+			</tr>
+			<tr id="nm_start_time_tr">
+				<td style="padding-left:6em">Start Time</td>
+				<td><input type="time" id="nm_start_time" value=<?php echo($nm_start_time)?>></td>
+			</tr>
+			<tr id="nm_end_time_tr">
+				<td style="padding-left:6em">End Time</td>
+				<td><input type="time" id="nm_end_time" value="<?php echo($nm_end_time)?>"></td>
+			</tr>
+		</table>
+	</div>
+	<div style="background-color:white; width:400px;";>
+		<h2>Color Settings</h2>
+		<table>
+		<tr>
+			<td>Color Inversion</td>
+			<td><input type="checkbox" id="nm_invert_color" <?php if($nm_invert_color == true){ echo "checked";} ?>></td>
+		</tr>
+		<tr>
+			<td>Greyscale</td>
+			<td><input type="checkbox" id="nm_grey" <?php if($nm_grey == true){ echo "checked";} ?>></td>
+		</tr>
+		<tr>
+			<td>Sepia</td>
+			<td><input type="checkbox" id="nm_sepia" <?php if($nm_sepia == true){ echo "checked";} ?>></td>
+		</tr>
+		</table>
+	</div>
+	<input type="submit" value="Save" id="nm_save">
+	
+	<script type='text/javascript src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'></script>
+	<script>
+	jQuery(document).ready(function($){
+		if(document.getElementById('nm_auto_set').checked == false){
+			$('#nm_set_manually_tr').hide();
+			$('#nm_start_time_tr').hide();
+			$('#nm_end_time_tr').hide();
+		}
+		if(document.getElementById('nm_set_manually').checked == false){
+			$('#nm_start_time_tr').hide();
+			$('#nm_end_time_tr').hide();
+		}
+	
+		$('#nm_auto_set').click(function(){
+			if(document.getElementById('nm_auto_set').checked){
+				$('#nm_set_manually_tr').show();
+				if(document.getElementById('nm_set_manually').checked){
+					$('#nm_start_time_tr').show();
+					$('#nm_end_time_tr').show();
+				}
+			}
+			else{
+				$('#nm_set_manually_tr').hide();
+				$('#nm_start_time_tr').hide();
+				$('#nm_end_time_tr').hide();
+			}
+		});
+		
+		$('#nm_set_manually').click(function(){
+			if(document.getElementById('nm_set_manually').checked){
+				$('#nm_start_time_tr').show();
+				$('#nm_end_time_tr').show();
+			}
+			else{
+				$('#nm_start_time_tr').hide();
+				$('#nm_end_time_tr').hide();
+			}
+		});
+		
+		$('#nm_save').click(function(){
+		/*
+			update_option("nm_auto_place", document.getElementById('nm_auto_place').checked);
+			update_option("nm_affect_pictures", document.getElementById('nm_affect_pictures').checked);
+			update_option("nm_auto_set", document.getElementById('nm_auto_set').checked);
+			update_option("nm_set_manually", document.getElementById('nm_set_manually').checked);
+			update_option("nm_start_time", document.getElementById('nm_start_time').value);
+			update_option("nm_end_time", document.getElementById('nm_end_time').value);
+			update_option("nm_invert_color", document.getElementById('nm_invert_color').checked);
+			update_option("nm_grey", document.getElementById('nm_grey').checked);
+			update_option("nm_sepia", document.getElementById('nm_sepia').checked);
+		*/
+		});
+	});
+	</script>
+	<?php
 }
 
 
